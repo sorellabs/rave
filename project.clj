5aca7e58118504564b5bd634004497fd862f8249
Quildreen Motta <quildreen.ribeiro@plansis.com.br>
2013-05-08 09:24:22 -0300

    Updates clojure dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject rave "0.1.2"
   :description "A wrapper on the Sardine WebDAV client."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.github.lookfirst/sardine "5.0"]])
